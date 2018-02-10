abdf2b21a84f3c8c2e5ccd8b5b0b169165980904
c4rlz <carly.ewasiuk94@gmail.com>
2018-02-09 19:21:37 -0800

    update size of phone, add more data

--- js/Heading.jsx
+++ js/Heading.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Heading = props => {
+  const checkDescription = () =>
+    props.description ? <p> {props.description} </p> : null;
   return (
     <div className="page-heading">
       <h1> {props.heading} </h1>
       <h4> {props.subheading} </h4>
+      {checkDescription()}
     </div>
   );
 };
--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import WheelReact from 'wheel-react';
-import Nav from './Nav';
+import React from 'react';
+// import WheelReact from 'wheel-react';
+// import Nav from './Nav';
 import Heading from './Heading';
-import ProgressBar from './ProgressBar';
+// import ProgressBar from './ProgressBar';
 
 const Landing = () => {
+  const description =
+    "I enjoy all things JavaScript. I'm constantly learning new things and questioning everything. I am a problem solver and I have a knack for finding bugs.";
   return (
     <div className="main-content landing">
       <Heading
         heading="Carly Ewasiuk"
         subheading="Front-End Developer & UX Enthusiast"
+        description={description}
       />
     </div>
   );
